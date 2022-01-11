679104a19bff5cdfc25ddee61778441312a0526f
NithyaAppannagaari <70969819+NithyaAppannagaari@users.noreply.github.com>
2022-01-11 13:41:51 -0800

    Add files via upload

---
+++ src/main/java/frc/robot/commands/StopIntaking.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Intake.IntakeState;
+
+public class StopIntaking extends CommandBase {
+  /** Creates a new StopIntaking. */
+  private Intake intake;
+  public StopIntaking(Intake intakeIn) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    intake = intakeIn;
+    addRequirements();
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    intake.setState(IntakeState.DOWN);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
