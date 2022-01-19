322ec0b07f8c66c74cd2426021c24eda32f753ae
NithyaAppannagaari <70969819+NithyaAppannagaari@users.noreply.github.com>
2022-01-18 18:27:30 -0800

    Update ExpelBalls.java

--- src/main/java/frc/robot/commands/ExpelBalls.java
+++ src/main/java/frc/robot/commands/ExpelBalls.java
@@ -25,7 +25,7 @@ public class ExpelBalls extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    storage.runMotor(false);
+    storage.runMotor(true);
   }
 
   // Called once the command ends or is interrupted.
