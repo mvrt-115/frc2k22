c4d4f3aa4508d10ec8178dbcee416cf720872385
Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>
2022-01-31 20:35:36 -0800

    attempting to steal my computer (sketchy)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,14 +5,19 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.IntakeBalls;
 import frc.robot.commands.SetRPM;
+import frc.robot.commands.StopIntaking;
 import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Intake;
 import frc.robot.util.Limelight;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -26,14 +31,19 @@ public class RobotContainer {
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
 
-  private final Limelight limelight = new Limelight();
-  private Shooter shooter = new Shooter(limelight);
+  //private final Limelight limelight = new Limelight();
+  //private Shooter shooter = new Shooter(limelight);
+
+  private Joystick joystick = new Joystick(0);
+  private JoystickButton joystickButton = new JoystickButton(joystick, 1);
+
+  private Intake intake = new Intake();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-    SmartDashboard.putData("Run Flywheel", new SetRPM(shooter));
+    //SmartDashboard.putData("Run Flywheel", new SetRPM(shooter));
   }
 
   /**
@@ -42,7 +52,9 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    joystickButton.whenPressed(new IntakeBalls(intake)).whenReleased(new StopIntaking(intake));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
