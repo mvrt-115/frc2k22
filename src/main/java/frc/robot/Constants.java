9fbbf8a6e989bfe6340a4499bee421967e00851b
Akash Palla <akash@mypalla.com>
2022-02-03 20:07:27 -0800

    Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
@@ -16,6 +17,8 @@ import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 public final class Constants {
     //OVERALL ROBOT CONSTANTS
     public static final boolean debugMode = true;
+
+    public static final double MAX_VOLTAGE = 10.0;
     
     public static final int kPIDIdx = 0;
     public static final int kTimeoutMs = 10;
@@ -24,6 +27,7 @@ public final class Constants {
     public static final SupplyCurrentLimitConfiguration kCurrentLimit = new SupplyCurrentLimitConfiguration(true, 40, 50, 3.8);
 
     public static class Drivetrain {
+
         // physical constants get from mech once drivetrain is done
         public static final int kTrackScrubFactor = 0;
         public static final double kTrackWidthInches = 0;
@@ -44,32 +48,78 @@ public final class Constants {
         public static final double kThrottleDeadband = 0.02;
         public static final double kWheelGain = 0.05;
         public static final double kWheelNonlinearity = 0.05;
+
     }
     
     public static class Intake{
 
-        public static final double kTICKS_TO_BOTTOM = 0;
+        public static final double kTICKS_TO_BOTTOM = -24399;
         // the # of ticks it takes to be at the bottom when pivoting
-        public static final double kTICKS_TO_TOP = 0; // the # of ticks it takes to be at the top when pivoting
+        public static final double kTICKS_TO_TOP = 23400; // the # of ticks it takes to be at the top when pivoting
 
-        public static final int kPIVOT_ID = 0; // change later
+        public static final int kPIVOT_ID = 34; // change later
         public static final int kROLLER_ID = 0; // change later
 
         public static final double kWHEELS_SPEED = 0.6;
 
         public static final double kPIVOT_SPEED = 0.4; // speed of intake when going up or down
-        public static final double kPIVOT_STOP_SPEED_WHEN_UP = 0.23; // speed of intake to keep it up
-        public static final double kPIVOT_STOP_SPEED_WHEN_DOWN = 0.0; // speed of intake to keep it down
+        public static final double kPIVOT_STOP_SPEED_WHEN_UP =0.13; // speed of intake to keep it up
+        public static final double kPIVOT_STOP_SPEED_WHEN_DOWN = -0.1; // speed of intake to keep it down
         //change later after testing
 
-        public static final double PIVOT_SPEED_WHEN_DOWN = 0;
-        public static final double PIVOT_SPEED_WHEN_UP = 0;
-        public static final double kMARGIN_OF_ERROR_TICKS = 0;
+        public static final double kMARGIN_OF_ERROR_TICKS = 4000; // constant
+
 
         // change values later
-        public static final double kP = 0.0;
-        public static final double kI = 0;
-        public static final double kD = 0;
-        public static final double kFF = 0.31;
+        public static final double kP = 0.015;
+        public static final double kI = 0.0;
+        public static final double kD = 0.001;//0.001;
+        public static final double kFF = 0;//-0.001;
+    }
+
+    public static class Limelight
+    {
+        public static final double MOUNT_ANGLE = 15;
+        public static final double Height_IN = 36;
+        public static final double TARGET_HEIGHT_IN = 104;
+    }
+
+    public static class Hood
+    {
+        public static final double MIN_ANG = 60;
+        public static final double MAX_ANG = 80;
+        public static final double P = 0.21;
+        public static final double I = 0;
+        public static final double D = 6.9;
+        public static final double F = 0.058;
+        public static final double RADIUS = 8.5;
+        public static final double MAX_VOLTAGE_COMPENSATION = 5;
+        public static final double GEAR_RATIO = 150.0/24;
+        public static final int ENCODER_TICKS = 4096;
+    }
+
+    public static class Flywheel
+    {
+        public static final double P = 0.21;
+        public static final double I = 0;
+        public static final double D = 6.9;
+        public static final double F = 0.058;
+        public static final double PHood = 0.21;
+        public static final double IHood = 0;
+        public static final double DHood = 6.9;
+        public static final double FHood = 0.058;
+        public static final double RADIUS = 2;
+        public static final double ACCEPTABLE_ERROR = 50;
+        public static final double MAX_VOLTAGE_COMPENSATION = 10;
+        public static final double TICKS_PER_REVOLUTION = 4096;
+        public static final int NUM_AVG = 5;
+        public static final double GEAR_RATIO = 50.0/36.0; // Can change
+    }
+
+    public static class Storage
+    {
+        public static final int kBreakBeamPort = 0;
+        public static final int kMotorID = 0;
+        public static final int kMotorSpeed = 0;
     }
 }
