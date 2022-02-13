8b44f51dbe3206f06703eeebb7e8d988b3828ab3
Sohan Shingade <55606409+sohan-shingade@users.noreply.github.com>
2022-02-12 19:59:08 -0800

    need to fix issues with command calling and setting state to not expelling when it ends

--- src/main/java/frc/robot/commands/ExpelBalls.java
+++ src/main/java/frc/robot/commands/ExpelBalls.java
@@ -4,36 +4,52 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.Storage;
+import frc.robot.subsystems.Storage.StorageState;
 
 public class ExpelBalls extends CommandBase {
   private Storage storage;
+  private int times;
+  private boolean inEndCommand = false;
   /** Creates a new ExpelBalls. */
   public ExpelBalls(Storage storageIn) {
     // Use addRequirements() here to declare subsystem dependencies.
     storage = storageIn;
+    times = 0;
     addRequirements(storage);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    Storage.currentState = Storage.StorageState.EXPELLING;
+    times++;
+    inEndCommand = false;
+    SmartDashboard.putNumber("num times command called", times);
+    storage.setState(StorageState.EXPELLING);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    inEndCommand = false;
+
+    //storage.runMotor();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    inEndCommand = true;
+    SmartDashboard.putBoolean("command is ending", inEndCommand);
+    storage.setState(StorageState.NOT_EXPELLING);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return true;
+    return storage.getBreakBeamLast().get() && storage.getBreakBeamFirst().get();
   }
 }
