fc66fc303079f0598fc9d093ecc077e17721fbea
MVRT Drive Laptop - Peepa <software@mvrt.com>
2022-04-11 19:23:28 -0700

    merged leds

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -44,7 +44,7 @@ public class RobotContainer {
   private final Climber climber = new Climber();
   private final Turret turret = new Turret(limelight);
   private final Shooter shooter = new Shooter(limelight, drivetrain, turret);
-  private final LEDs led = new LEDs();
+  //private final LEDs led = new LEDs();
 
   // climber operator manual buttons
   private JoystickButton extend;
@@ -141,7 +141,7 @@ public class RobotContainer {
     // alignDrivetrain.whenPressed(new AlignIntakeToBall(drivetrain, true)).whenReleased(new JoystickDrive(drivetrain, this::getThrottle, this::getWheel, quickturn::get));
     drivetrain.setDefaultCommand(new JoystickDrive(drivetrain, intake, 
     this::getThrottle, this::getWheel, quickturn::get));
-    led.setDefaultCommand(new LEDCommand(led, turret, shooter, climber, storage, intake));
+    //led.setDefaultCommand(new LEDCommand(led, turret, shooter, climber, storage, intake));
 
     //storage.setDefaultCommand(new TrackBalls(storage, shooter));
     //turret.setDefaultCommand(new FindTarget(turret));
@@ -279,7 +279,7 @@ public class RobotContainer {
   }
 
   public void disabledPeriodic() {
-    led.setFullLength(led.kMVRTPurple);
-    led.setFullLengthTurret(led.kMVRTPurple);
+    //led.setFullLength(led.kMVRTPurple);
+    //led.setFullLengthTurret(led.kMVRTPurple);
   }
 }
